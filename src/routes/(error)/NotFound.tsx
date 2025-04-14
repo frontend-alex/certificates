63b8a7ec2b9765e3ea7f5765c5ccc397509a0d9b
I554344 <alexmmech@gmail.com>
2025-04-14 19:26:56 +0200

    final changes

--- src/routes/(error)/NotFound.tsx
+++ src/routes/(error)/NotFound.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button"
+import { useNavigate } from "react-router-dom"
+
 const NotFound = () => {
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      
+    <div className="flex flex-col gap-5 justify-center items-center h-[50dvh]">
+      <h1 className="text-5xl lg:text-9xl font-corm">Ugh ohhhh.</h1>
+      <p className="text-center max-w-[300px]">Hmm.... the page you are looking for doesnt seems to be on here. </p>
+      <Button onClick={() => navigate(-1)} variant={'outline'}>Go Back</Button>
     </div>
   )
 }
