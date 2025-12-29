3fca62a2a7baec54756d17c83ea5ece0d04a18a9
Aleksander Ivanov <62203063+frontend-alex@users.noreply.github.com>
2025-12-29 16:03:53 +0100

    Remove ThemeToggle component from Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,6 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between gap-5 max-w-[90em] mx-auto p-5 w-full'>
       <h1 className="font-bold text-xl">AI.</h1>
-      <ThemeToggle/>
     </div>
   )
 }
